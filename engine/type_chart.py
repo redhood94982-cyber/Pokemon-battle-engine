76b00aa2e73f8bd91205dab67dfadcf78e0821a1
redhood94982-cyber <redhood94982@gmail.com>
2026-07-28 14:23:43 -0500

    Update type_chart.py
    add normal and fire type chart

--- engine/type_chart.py
+++ engine/type_chart.py
@@ -5,4 +5,21 @@ type_chart.py
 Generation 6+ type effectiveness.
 """
 
-TYPE_CHART = {}
+TYPE_CHART = {
+    "Normal": {
+        "Rock": 0.5,
+        "Ghost": 0.0,
+        "Steel": 0.5,
+    },
+
+    "Fire": {
+        "Fire": 0.5,
+        "Water": 0.5,
+        "Grass": 2.0,
+        "Ice": 2.0,
+        "Bug": 2.0,
+        "Rock": 0.5,
+        "Dragon": 0.5,
+        "Steel": 2.0,
+    },
+}
