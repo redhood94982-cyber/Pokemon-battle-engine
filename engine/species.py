ad101a27d7a144c481874f042c21384f0d2f267b
redhood94982-cyber <redhood94982@gmail.com>
2026-07-29 13:42:03 -0500

    Add files via upload

---
+++ engine/species.py
@@ -0,0 +1,39 @@
+"""
+species.py
+
+Species database for the Pokémon Battle Engine.
+
+Development Rule:
+- Add complete evolution families.
+- Include regional forms that belong to those families.
+- Only store species names until mechanics require additional data.
+"""
+
+SPECIES = {
+    # Gastly Evolution Family
+    "Gastly": {},
+    "Haunter": {},
+    "Gengar": {},
+
+    # Litten Evolution Family
+    "Litten": {},
+    "Torracat": {},
+    "Incineroar": {},
+
+    # Sneasel Evolution Family
+    "Sneasel": {},
+    "Weavile": {},
+    "Sneasel (Hisui)": {},
+    "Sneasler": {},
+
+    # Cresselia
+    "Cresselia": {},
+
+    # Wattrel Evolution Family
+    "Wattrel": {},
+    "Kilowattrel": {},
+
+    # Poltchageist Evolution Family
+    "Poltchageist": {},
+    "Sinistcha": {},
+}
