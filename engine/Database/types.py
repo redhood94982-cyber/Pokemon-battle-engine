dd62270df79450b55acb16430b6e71c8c95a7320
redhood94982-cyber <redhood94982@gmail.com>
2026-07-29 13:49:48 -0500

    Add files via upload

---
+++ engine/Database/types.py
@@ -0,0 +1,26 @@
+"""
+types.py
+
+Official Pokémon types supported by the Pokémon Battle Engine.
+"""
+
+TYPES = [
+    "Normal",
+    "Fire",
+    "Water",
+    "Electric",
+    "Grass",
+    "Ice",
+    "Fighting",
+    "Poison",
+    "Ground",
+    "Flying",
+    "Psychic",
+    "Bug",
+    "Rock",
+    "Ghost",
+    "Dragon",
+    "Dark",
+    "Steel",
+    "Fairy",
+]
